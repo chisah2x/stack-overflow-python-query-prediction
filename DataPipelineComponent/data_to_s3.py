883d2e16d82f25b55e5514d5549cab1720cec91b
Chirag Sahu <chiragsahu257@gmail.com>
2023-03-26 12:31:09 +0530

    commiting 1st component of project- DATA PIPELINE

---
+++ DataPipelineComponent/data_to_s3.py
@@ -0,0 +1,25 @@
+from utils import *
+import os
+from s3_sync import S3Sync
+from dotenv import load_dotenv
+load_dotenv()
+
+class DataDumpS3:
+    def __init__(self, file_path):
+        self.bucket_name = os.getenv("AWS_S3_BUCKET")
+        self.file_path= file_path
+    
+    def dump_data(self):
+        for file in os.listdir(self.file_path):
+            if file.endswith(".xml"):
+                data_dir=os.path.join(os.getcwd(),self.file_path)
+                print(data_dir)
+                s3_uri=f"s3://{self.bucket_name}/stack_overflow_data"
+                S3Sync.s3_json_folder_sync(data_dir,s3_uri)
+            else:
+                return "Data failed to store in s3 bucket"
+
+if __name__=="__main__":
+    print(DataDumpS3("main_data").dump_data())
+
+    
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # stack-overflow-python-query-prediction
+
+1st component: Data Pipeline Component:
+
+data.xml is a semi-structure data which is scraped from the website stackoverflow.com
+
+following steps need to perform for creating data pipeline component.
+
+i) creation and activation of virtual env 
+ii) installing all deps
+iii) create iam new user with s3,ec2 full access service then create access key
+iv) creat bucket 
+v) interact with falsk and s3 and getting all the data to webpage. 
+vi) Create Ec2 machine
+vii) connect to ec2 machine:
+viii) create account on rapid api and deploy the hosted link
+
+completed first component.
+
